b3c881c2b03a02724d769dde42095cbb611dad3b
Lakeisha McCree <lmccree215@icloud.com>
2020-02-04 03:02:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,8 +5,12 @@ def hello_t(array)
       yield(array[i])
       i = i + 1
     end
+    
     array
-end
+  else 
+    puts "Hey! No block was given!"
+  end
+end 
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
